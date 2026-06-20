a5823c0603aa2f4b1180edaec9779700d5c0df60
oonepower <onepower967777543818@gmail.com>
2026-06-20 22:12:53 +0300

    Add Capacitor placeholder script

---
+++ www/capacitor.js
@@ -0,0 +1,10 @@
+/* Capacitor Placeholder Script
+   This script is auto-injected by Capacitor when building for Android
+   For development, this is a stub that prevents errors
+*/
+
+window.Capacitor = {
+    isPluginAvailable: function(plugin) {
+        return false;
+    }
+};
